cf49273e2bc9889644c0d92cea4f8a531943896a
Alexey <lexa7.03.1994@yandex.ru>
2023-01-10 21:02:36 +0700

    Form add post

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,27 @@
 <template>
     <div class="app">
-        <form>
+        <form @submit.prevent>
             <h4>Создание поста</h4>
-            <input class="input" type="text" placeholder="Название">
-            <input class="input" type="text" placeholder="Описание">
-            <button class="btn">Создать</button>
+            <input 
+                v-bind:value="title" 
+                @input="title = $event.target.value"
+                class="input"
+                type="text"
+                placeholder="Название
+            ">
+            <input 
+                v-bind:value="body" 
+                @input="body = $event.target.value"
+                class="input" 
+                type="text"
+                placeholder="Описание"
+            >
+            <button 
+                class="btn" 
+                @click="createPost"
+                >
+                Создать
+            </button>
         </form>
         <div class="post" v-for:="post in posts">
             <div><strong>Название: </strong>{{ post.title }}</div>
@@ -22,11 +39,22 @@ export default {
                 { id: 2, title: 'JS 2', body: 'Описание поста 2' },
                 { id: 3, title: 'JS 3', body: 'Описание поста 3' },
                 { id: 4, title: 'JS 4', body: 'Описание поста 4' },
-            ]
+            ],
+            title: '',
+            body: ''
         }
     },
     methods: {
-
+        createPost() {
+            const newPost = {
+                id: Date.now(),
+                title: this.title,
+                body: this.body,
+            }
+            this.posts.push(newPost);
+            this.title = '';
+            this.body = '';
+        },
     },
 }
 </script>
